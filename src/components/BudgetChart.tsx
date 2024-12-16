f6fcf0409cdf7930cf581cb146d84dd767b07e33
Arabhaya <r.arabhaya@gmail.com>
2024-12-16 22:52:09 +0530

    Configured role based Dashboard stats

--- src/components/BudgetChart.tsx
+++ src/components/BudgetChart.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Bar } from 'react-chartjs-2';
+import React from "react";
+import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -7,8 +7,8 @@ import {
   BarElement,
   Title,
   Tooltip,
-  Legend
-} from 'chart.js';
+  Legend,
+} from "chart.js";
 
 ChartJS.register(
   CategoryScale,
@@ -19,26 +19,26 @@ ChartJS.register(
   Legend
 );
 
-const budgetData = {
-  labels: ['Total'],
-  datasets: [
-    {
-      label: 'Sanctioned Budget',
-      data: [220000],
-      backgroundColor: '#0ea5e9',
-    },
-    {
-      label: 'Released Budget',
-      data: [20000],
-      backgroundColor: '#10b981',
-    },
-    {
-      label: 'Pending Budget',
-      data: [180000],
-      backgroundColor: '#f97316',
-    }
-  ]
-};
+// const budgetData = {
+//   labels: ["Total"],
+//   datasets: [
+//     {
+//       label: "Sanctioned Budget",
+//       data: [220000],
+//       backgroundColor: "#0ea5e9",
+//     },
+//     {
+//       label: "Released Budget",
+//       data: [20000],
+//       backgroundColor: "#10b981",
+//     },
+//     {
+//       label: "Pending Budget",
+//       data: [180000],
+//       backgroundColor: "#f97316",
+//     },
+//   ],
+// };
 
 const options = {
   responsive: true,
@@ -47,22 +47,22 @@ const options = {
       beginAtZero: true,
       title: {
         display: true,
-        text: 'Amount (in Lacs)'
-      }
-    }
+        text: "Amount (in Crore)",
+      },
+    },
   },
   plugins: {
     legend: {
-      position: 'bottom' as const,
+      position: "bottom" as const,
     },
     title: {
       display: true,
-      text: 'Budget Overview (in Lacs)'
-    }
-  }
+      text: "Budget Overview (in Lacs)",
+    },
+  },
 };
 
-export default function BudgetChart() {
+export default function BudgetChart({ budgetData }) {
   return (
     <div className="bg-white p-4 rounded-xl shadow-sm border border-gray-100">
       <Bar data={budgetData} options={options} />
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,50 +10,53 @@ import { endpoint } from "../utils/dataSet.ts";
 import { useEntities } from "../context/EntityContext.tsx";
 import { DepartmentPieChart } from "../components/dashboardPieChart.tsx";
 import { DepartmentBarChart } from "../components/DepartmentBarChart.tsx";
-import {Download} from "lucide-react";
-
-const projectStatusLabels = [
-  "In Planning",
-  "In Progress",
-  "On Hold",
-  "Delayed",
-  "Completed",
-];
+import { Download } from "lucide-react";
 
 export default function HomePage() {
-  const [stats, setStats] = useState({});
-  const [projectStatus, setProjectStatus] = useState([]);
+  const { user, entities, reloadEntities } = useEntities();
+  const [dashboardStats, setDashboardStats] = useState({});
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
   const [departmentData, setDepartmentData] = useState([]);
+
   const pieChartRef = useRef(null);
   const barChartRef = useRef(null);
-  const {entities} = useEntities();
   // Fetch Overall Stats Data
-  const fetchStatsData = async () => {
+
+  const fetchDashboardStats = async () => {
+    if (!user?.entityId || !user?.entityTypeId) {
+      setError("Entity ID or Entity Type ID is missing.");
+      return;
+    }
+
+    setLoading(true);
+    setError("");
+
+    const paramsData = {
+      entityId: user.entityId,
+      entityTypeId: user.entityTypeId,
+    };
+    // console.log("paramsData", paramsData);
     try {
       const response = await axios.get(
-        `${endpoint}/api/stats/budget-overview`,
-        {
-          headers: {
-            "ngrok-skip-browser-warning": "true",
-          },
-        }
+        `${endpoint}/api/entity-overview`,
+        user.userRole == 1 ? {} : { params: paramsData }
       );
-      // console.log(response.data);
-      setStats(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
-   // Export Chart as PNG
-   const exportChartAsPNG = (ref, filename) => {
-    if (ref.current) {
-      html2canvas(ref.current, { useCORS: true }).then((canvas) => {
-        const link = document.createElement("a");
-        link.download = `${filename}.png`;
-        link.href = canvas.toDataURL("image/png");
-        link.click();
-      });
+      if (response.data.success) {
+        console.log("response.data.data", response.data.data);
+        setDashboardStats(response.data.data);
+      } else {
+        setError("Failed to fetch data. Please try again.");
+      }
+    } catch (err) {
+      setError(
+        err.response?.data?.message || "An error occurred while fetching data."
+      );
+      console.error("Error fetching dashboard stats:", err);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -67,34 +70,68 @@ export default function HomePage() {
           },
         }
       );
-      console.log(response.data);
-      setDepartmentData(response.data.data);
+      setDepartmentData(response.data.data || []);
     } catch (error) {
       console.error(error);
-    }
-  };
-
-
-  const fetchProjectStatus = async () => {
-    try {
-      const response = await axios.get(`${endpoint}/api/stats/project-status`);
-      // console.log(response.data.data);
-      // console.log(response.data.data.filter((item) => item.project_status !== null ).map((item) => item.count));
-      const values = response.data.data
-        .filter((item) => item.project_status !== null)
-        .map((item) => item.count);
-
-      setProjectStatus(values);
-    } catch (error) {
-      console.error(error);
+      setDepartmentData([]);
     }
   };
 
   useEffect(() => {
-    fetchStatsData();
+    if (user) {
+      fetchDashboardStats();
+    }
     fetchDepartmentData();
-    fetchProjectStatus();
-  }, []);
+  }, [user]);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="text-red-500">{error}</div>
+      </div>
+    );
+  }
+
+  const projectStatusLabels = dashboardStats?.projectStatusDistribution?.map(
+    (data) => data.status
+  );
+
+  const projectStatusCounts = dashboardStats?.projectStatusDistribution?.map(
+    (data) => data.count
+  );
+  const budgetData = {
+    labels: ["Total"],
+    datasets: [
+      {
+        label: "Sanctioned Budget",
+        data: [dashboardStats?.totalApprovedBudget || 0],
+        backgroundColor: "#0ea5e9",
+      },
+      {
+        label: "Released Budget",
+        data: [dashboardStats?.totalReleasedFunds || 0],
+        backgroundColor: "#10b981",
+      },
+      {
+        label: "Expenditure",
+        data: [dashboardStats?.totalExpenditure || 0],
+        backgroundColor: "#FB1216",
+      },
+      {
+        label: "Pending Budget",
+        data: [dashboardStats?.totalPendingBudget || 0],
+        backgroundColor: "#f97316",
+      },
+    ],
+  };
 
   return (
     <div className="pb-20 pt-16">
@@ -108,65 +145,68 @@ export default function HomePage() {
         <div className="grid grid-cols-1 gap-4 mb-6">
           <StatsCard
             label="Total Projects"
-            value={stats.totalProjects}
+            value={dashboardStats?.totalProjects || 0}
             icon={Building2}
           />
 
           <StatsCard
             label="Total Budget (Crores)"
-            value={"₹" + stats?.totalBudget?.approved}
+            value={`₹${dashboardStats?.cumulativeTotalBudget || 0}`}
             icon={IndianRupee}
           />
 
           <StatsCard
             label="In Progress Projects"
-            value={stats?.activeProjects?.count}
+            value={dashboardStats?.inProgressProjects || 0}
             icon={Activity}
           />
-          <StatsCard label="Executing Agencies" value={entities?.length} icon={Users} />
+          <StatsCard
+            label="Executing Agencies"
+            value={
+              user?.userRole == 3 || user?.userRole == 4
+                ? dashboardStats?.totalRelatedEntities || 0
+                : entities?.filter((entity) => entity.entity_type === 2)
+                    .length || 0
+            }
+            icon={Users}
+          />
         </div>
 
         <div className="space-y-6">
           <ProjectStatusChart
-            labels={["In Progress", "Completed", "On Hold", "In Planning"]}
-            counts={projectStatus}
+            labels={projectStatusLabels}
+            counts={projectStatusCounts}
           />
-          <BudgetChart />
+          <BudgetChart budgetData={budgetData} />
         </div>
-        {/* Department-wise Project Count - Pie Chart */}
-      <div className="rounded-lg bg-white shadow mt-6">
-        <div className="px-6 py-5 flex justify-between items-center">
-          <h3 className="text-lg font-medium leading-6 text-gray-900">
-            Department-wise Project Count
-          </h3>
-         
-        </div>
-        <div ref={pieChartRef} className="p-6">
-          <DepartmentPieChart data={departmentData} />
-        </div>
+
+        {user?.userRole == 1 && (
+          <>
+            {/* Department-wise Project Count - Pie Chart */}
+            <div className="rounded-lg bg-white shadow mt-6">
+              <div className="px-6 py-5 flex justify-between items-center">
+                <h3 className="text-lg font-medium leading-6 text-gray-900">
+                  Department-wise Project Count
+                </h3>
+              </div>
+              <div ref={pieChartRef} className="p-6">
+                <DepartmentPieChart data={departmentData} />
+              </div>
+            </div>
+            {/* Department-wise Project Count - Bar Chart */}
+            <div className="rounded-lg bg-white shadow mt-6">
+              <div className="px-6 py-5 flex justify-between items-center">
+                <h3 className="text-lg font-medium leading-6 text-gray-900">
+                  Department-wise Project Count <br /> (Bar Chart)
+                </h3>
+              </div>
+              <div ref={barChartRef} className="">
+                <DepartmentBarChart data={departmentData} />
+              </div>
+            </div>
+          </>
+        )}
       </div>
-      {/* Department-wise Project Count - Bar Chart */}
-      <div className="rounded-lg bg-white shadow mt-6">
-        <div className="px-6 py-5 flex justify-between items-center">
-          <h3 className="text-lg font-medium leading-6 text-gray-900">
-            Department-wise Project Count <br/> (Bar Chart)
-          </h3>
-          {/* <button
-            onClick={() =>
-              exportChartAsPNG(barChartRef, "DepartmentWiseBarChart")
-            }
-            className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-          >
-            <Download className="h-4 w-4 mr-1" />
-            Export as PNG
-          </button> */}
-        </div>
-        <div ref={barChartRef} className="">
-          <DepartmentBarChart data={departmentData} />
-        </div>
-      </div>
-      </div>
-      
     </div>
   );
 }
